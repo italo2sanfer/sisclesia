0cee41ee8dc843042c3ac2f65df1d431c3d1f35a
Italo Sanfer <iferreira@iff.edu.br>
2017-04-24 11:27:40 -0300

    Trocando nome modulo de core para principal; adicionando ignore e setting_exemplo

--- sisclesia/urls.py
+++ sisclesia/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-#    url(r'^core/', include('core.urls')),
-    url(r'', include('core.urls')),
+#    url(r'^core/', include('principal.urls')),
+    url(r'', include('principal.urls')),
     url(r'^admin/', admin.site.urls),
 ]
